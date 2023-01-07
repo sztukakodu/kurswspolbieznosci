a5dd127fcb6ef1478db3b3d8650077d80b6a2c76
Darek Mydlarz <dmydlarz@gmail.com>
2023-01-07 22:33:33 +0100

    Initialize project

---
+++ src/main/java/pl/sztukakodu/concurrency/App.java
@@ -0,0 +1,7 @@
+package pl.sztukakodu.concurrency;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/pl/sztukakodu/concurrency/week4/MyFixedThreadPoolTest.java
@@ -0,0 +1,12 @@
+package pl.sztukakodu.concurrency.week4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFixedThreadPoolTest {
+    @Test
+    public void helloTest() {
+        assertEquals("Hello 5", "Hello " + 5);
+    }
+} 
